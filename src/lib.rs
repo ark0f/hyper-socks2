0d27abf87eb16df9132aed79de4d58853645b7b5
ark0f <arsenylyashenko.3@gmail.com>
2020-01-04 17:01:13 +0300

    Update to async/await

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
-//! The reborn of the SOCKS4/5 connector for Hyper library
+//! A SOCKS5 connector for Hyper library
 //!
 //! # Example
-//! ```
-//! use hyper::{client::Client, Body};
-//! use hyper_socks2::Proxy;
+//! ```no_run
+//! # use std::error::Error;
+//! # fn hidden() -> Result<(), Box<dyn Error>> {
 //!
-//! # use hyper_socks2::Error;
-//! # fn hidden() -> Result<(), Error> {
-//! let proxy = Proxy::Socks5 {
-//!     addrs: "your.socks5.proxy:1080",
+//! use hyper::{client::Client, Body};
+//! use hyper_socks2::SocksConnector;
+//!
+//! let proxy = SocksConnector {
+//!     proxy_addr: "your.socks5.proxy:1080",
 //!     auth: None,
 //! };
 //!
@@ -16,118 +17,189 @@
 //! let proxy = proxy.with_tls()?;
 //!
 //! let client = Client::builder().build::<_, Body>(proxy);
+//!
 //! # Ok(())
 //! # }
 //! ```
 //!
 //! # Features
-//! * `tls` feature enabled by default. It adds TLS support using `hyper-tls`.
+//! * `tls` feature is enabled by default. It adds TLS support using `hyper-tls`.
 
-use futures::{Async, Future, Poll};
-use hyper::client::connect::{Connect, Connected, Destination};
-#[cfg(feature = "tls")]
-use native_tls::TlsConnector;
-use socks::{Socks4Stream, Socks5Stream};
-use std::{io, net::ToSocketAddrs};
-use tokio::{net::TcpStream, reactor::Handle};
+use async_socks5::{AddrKind, Auth};
+use futures::task::{Context, Poll};
+use http::uri::Scheme;
+use hyper::{service::Service, Uri};
+use hyper_tls::HttpsConnector;
+use std::{future::Future, io, pin::Pin};
+use tokio::net::{TcpStream, ToSocketAddrs};
 
-#[cfg(feature = "tls")]
-pub use {hyper_tls::HttpsConnector, native_tls::Error};
-
-/// A future with ready TCP stream
-pub struct Connection {
-    inner: Option<Poll<(TcpStream, Connected), io::Error>>,
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("{}", &0)]
+    Socks(
+        #[from]
+        #[source]
+        async_socks5::Error,
+    ),
+    #[error("{}", &0)]
+    Io(
+        #[from]
+        #[source]
+        io::Error,
+    ),
+    #[error("Missing host")]
+    MissingHost,
 }
 
-impl Connection {
-    fn result(result: Result<(TcpStream, Connected), io::Error>) -> Self {
-        let inner = Some(result.map(Async::Ready));
-        Connection { inner }
-    }
-
-    fn error(error: io::Error) -> Self {
-        Connection::result(Err(error))
-    }
+#[derive(Debug, Clone)]
+pub struct SocksConnector<T> {
+    pub proxy_addr: T,
+    pub auth: Option<Auth>,
 }
 
-impl Future for Connection {
-    type Item = (TcpStream, Connected);
-    type Error = io::Error;
-
-    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        self.inner.take().expect("cannot take Poll twice")
-    }
-}
-
-/// A SOCKS4/5 proxy information and TCP connector
-#[derive(Debug, Clone, Eq, PartialEq)]
-pub enum Proxy<T: ToSocketAddrs> {
-    Socks4 { addrs: T, user_id: String },
-    Socks5 { addrs: T, auth: Option<Auth> },
-}
-
-/// An authentication information
-#[derive(Debug, Clone, Eq, PartialEq)]
-pub struct Auth {
-    pub user: String,
-    pub pass: String,
-}
-
-impl<T> Proxy<T>
-where
-    T: ToSocketAddrs,
-{
+impl<T> SocksConnector<T> {
     /// Create a new connector with TLS support
     #[cfg(feature = "tls")]
-    pub fn with_tls(self) -> Result<HttpsConnector<Self>, Error> {
-        let args = (self, TlsConnector::new()?);
+    pub fn with_tls(self) -> Result<HttpsConnector<Self>, hyper_tls::native_tls::Error> {
+        let args = (self, hyper_tls::native_tls::TlsConnector::new()?.into());
         Ok(HttpsConnector::from(args))
     }
 }
 
-impl<T> Connect for Proxy<T>
+impl<T> SocksConnector<T>
 where
-    T: ToSocketAddrs + Send + Sync,
+    T: ToSocketAddrs,
 {
-    type Transport = TcpStream;
-    type Error = io::Error;
-    type Future = Connection;
+    async fn call_async(self, target_addr: Uri) -> Result<TcpStream, Error> {
+        let host = target_addr
+            .host()
+            .map(str::to_string)
+            .ok_or(Error::MissingHost)?;
+        let port =
+            target_addr
+                .port_u16()
+                .unwrap_or(if target_addr.scheme() == Some(&Scheme::HTTPS) {
+                    443
+                } else {
+                    80
+                });
+        let target_addr = AddrKind::Domain(host, port);
 
-    fn connect(&self, dst: Destination) -> Self::Future {
-        let scheme = dst.scheme();
-        let port = if let Some(p) = dst.port() {
-            p
-        } else if scheme == "http" {
-            80
-        } else if scheme == "https" {
-            443
-        } else {
-            return Connection::error(io::Error::new(io::ErrorKind::InvalidInput, "missing port"));
-        };
-        let target = (dst.host(), port);
+        let mut stream = TcpStream::connect(self.proxy_addr).await?;
+        let _ = async_socks5::connect(&mut stream, target_addr, self.auth).await?;
+        Ok(stream)
+    }
+}
 
-        let res = match self {
-            Proxy::Socks4 {
-                ref addrs,
-                ref user_id,
-            } => Socks4Stream::connect(addrs, target, &user_id).map(Socks4Stream::into_inner),
-            Proxy::Socks5 {
-                ref addrs,
-                ref auth,
-            } => {
-                let res = match auth {
-                    Some(auth) => {
-                        Socks5Stream::connect_with_password(addrs, target, &auth.user, &auth.pass)
-                    }
-                    None => Socks5Stream::connect(addrs, target),
-                };
-                res.map(Socks5Stream::into_inner)
+impl<T> Service<Uri> for SocksConnector<T>
+where
+    T: ToSocketAddrs + Clone + Send + Sync + 'static,
+{
+    type Response = TcpStream;
+    type Error = Error;
+    type Future = SocksFuture;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: Uri) -> Self::Future {
+        let this = self.clone();
+        SocksFuture {
+            fut: Box::pin(async move { this.call_async(req).await }),
+        }
+    }
+}
+
+type ConnectResult = Result<TcpStream, Error>;
+
+pub struct SocksFuture {
+    fut: Pin<Box<dyn Future<Output = ConnectResult> + Send>>,
+}
+
+impl Future for SocksFuture {
+    type Output = ConnectResult;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        Pin::new(&mut self.fut).poll(cx)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hyper::{Body, Client};
+
+    const PROXY_ADDR: &str = "127.0.0.1:1080";
+
+    enum ConnectorKind {
+        Http,
+        Https,
+    }
+
+    struct Tester {
+        connector_kind: ConnectorKind,
+        auth: Option<Auth>,
+    }
+
+    impl Tester {
+        fn http() -> Self {
+            Self {
+                connector_kind: ConnectorKind::Http,
+                auth: None,
             }
-        };
-        let res = res
-            .and_then(|stream| TcpStream::from_std(stream, &Handle::default()))
-            .map(|stream| (stream, Connected::new()));
+        }
 
-        Connection::result(res)
+        fn https() -> Self {
+            Self {
+                connector_kind: ConnectorKind::Https,
+                auth: None,
+            }
+        }
+
+        fn with_auth(mut self) -> Self {
+            self.auth = Some(Auth {
+                username: "hyper".to_string(),
+                password: "proxy".to_string(),
+            });
+            self
+        }
+
+        async fn test(self) {
+            let socks = SocksConnector {
+                proxy_addr: PROXY_ADDR,
+                auth: self.auth,
+            };
+
+            let fut = match self.connector_kind {
+                ConnectorKind::Http => Client::builder()
+                    .build::<_, Body>(socks)
+                    .get(Uri::from_static("http://google.com")),
+                ConnectorKind::Https => Client::builder()
+                    .build::<_, Body>(socks.with_tls().unwrap())
+                    .get(Uri::from_static("https://google.com")),
+            };
+            let _ = fut.await.unwrap();
+        }
+    }
+
+    #[tokio::test]
+    async fn http_no_auth() {
+        Tester::http().test().await
+    }
+
+    #[tokio::test]
+    async fn https_no_auth() {
+        Tester::https().test().await
+    }
+
+    #[tokio::test]
+    async fn http_auth() {
+        Tester::http().with_auth().test().await
+    }
+
+    #[tokio::test]
+    async fn https_auth() {
+        Tester::https().with_auth().test().await
     }
 }
